3216d4609938d82295f4be4375292840d2551cdd
Mohamed Sayed <mohameddsayedd17@gmail.com>
2026-07-25 02:20:14 +0300

    Upgrade CHECK

--- src/test/java/factory/BrowserOptionsFactory.java
+++ src/test/java/factory/BrowserOptionsFactory.java
@@ -42,7 +42,7 @@ public class BrowserOptionsFactory {
 
     public static EdgeOptions getEdgeOptions() {
         EdgeOptions options = new EdgeOptions();
-        options.addArguments("-headless");
+        options.addArguments("--headless=new");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
@@ -53,6 +53,15 @@ public class BrowserOptionsFactory {
         options.addArguments("--ignore-certificate-errors");
         options.addArguments("--proxy-server=direct://");
         options.addArguments("--proxy-bypass-list=*");
+        // Prevents Edge's first-run experience, which blocks headless startup
+        // under service accounts like SYSTEM and causes the DevToolsActivePort crash
+        options.addArguments("--no-first-run");
+        options.addArguments("--no-default-browser-check");
+        options.addArguments("--disable-background-networking");
+        // Unique profile dir per run avoids profile-lock conflicts when
+        // Chrome/Edge/Firefox run in parallel (testNG.xml uses parallel="tests")
+        options.addArguments("--user-data-dir=" + System.getProperty("java.io.tmpdir")
+                + "edge-profile-" + System.nanoTime());
 
         return options;
     }
